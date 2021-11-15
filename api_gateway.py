78944e6b8f6c1a381731ed88f7e60a9268936f64
Justin Cheung <i.am@justincue.ng>
2021-11-15 21:09:00 +0800

    [feat] add redis for message queue

--- api_gateway.py
+++ api_gateway.py
@@ -1,7 +1,10 @@
 import flask
 
 flask_app = flask.Flask(__name__)
+redis_conn = redis.Redis(host='message_queue', port=6379)
+
 
 
 if __name__ == '__main__':
     flask_app.run(host='0.0.0.0', debug=True, port=15000)
+    
